d3a3e9f67a98212158e986176d72057edda301fa
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-07-06 11:21:45 -0600

    Adding constants

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import constants
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
+
