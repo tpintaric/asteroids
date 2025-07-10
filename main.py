8093e9ce191c9b3e6593563e6c82b408316ac74e
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-07-10 07:33:59 -0600

    move pygame shit

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
