e346e62b61457a09fd33fcfb485d60d825c4ee30
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-07-10 12:00:47 -0600

    Player moves and rotates now

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 def main():
@@ -15,10 +16,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_HEIGHT /2, SCREEN_WIDTH / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return        screen.fill("black")
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
