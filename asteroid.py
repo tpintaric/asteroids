8f8b0cba70a7dc6c0c72f699f541397c1ad7e083
Tomek <19194950+tpintaric@users.noreply.github.com>
2025-07-12 15:09:32 -0600

    Included asteroids into the game

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import *
 
 pygame.init()
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
     Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
-    updatable.add(player)
-    drawable.add(player)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    asteroid = AsteroidField()
 
     while True:
         for event in pygame.event.get():
